0ce1ed6ea33bc0e753f192df0ff206274dd9077a
Tihomir Georgiev <georgiev.tm@abv.bg>
2025-11-29 13:29:51 +0100

    added modified code to use PostgreSql as a default Sql

--- Neon.CRM.WebApp/Program.cs
+++ Neon.CRM.WebApp/Program.cs
@@ -5,9 +5,9 @@ using Neon.CRM.WebApp.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("NeonCRMConnection") ?? throw new InvalidOperationException("Connection string 'NeonCRMConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
