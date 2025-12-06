ee3779778fd6daf253c461005fdf2f9b62c3eaae
Tihomir Georgiev <georgiev.tm@abv.bg>
2025-12-06 11:45:33 +0100

    modified Index razor page

--- Neon.CRM.WebApp/Pages/Customers/Index.cshtml.cs
+++ Neon.CRM.WebApp/Pages/Customers/Index.cshtml.cs
@@ -8,23 +8,22 @@ using Microsoft.EntityFrameworkCore;
 using Neon.CRM.WebApp.Data;
 using Neon.CRM.WebApp.Data.Models;
 
-namespace Neon.CRM.WebApp.Pages.Customers
+namespace Neon.CRM.WebApp.Pages.Customers;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly Neon.CRM.WebApp.Data.ApplicationDbContext _context;
+
+    public IndexModel(Neon.CRM.WebApp.Data.ApplicationDbContext context)
     {
-        private readonly Neon.CRM.WebApp.Data.ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public IndexModel(Neon.CRM.WebApp.Data.ApplicationDbContext context)
-        {
-            _context = context;
-        }
+    public IList<Customer> Customer { get;set; } = default!;
 
-        public IList<Customer> Customer { get;set; } = default!;
-
-        public async Task OnGetAsync()
-        {
-            Customer = await _context.Customers
-                .Include(c => c.Agent).ToListAsync();
-        }
+    public async Task OnGetAsync()
+    {
+        Customer = await _context.Customers
+            .Include(c => c.Agent).ToListAsync();
     }
 }
