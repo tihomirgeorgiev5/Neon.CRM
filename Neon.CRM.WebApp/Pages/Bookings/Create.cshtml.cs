d62acd7f2dbcabb3abb2f4603bfeb782891f96f6
Tihomir Georgiev <georgiev.tm@abv.bg>
2025-12-08 10:19:16 +0100

    added new folder Bookings with razor page

---
+++ Neon.CRM.WebApp/Pages/Bookings/Create.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Neon.CRM.WebApp.Data;
+using Neon.CRM.WebApp.Data.Models;
+
+namespace Neon.CRM.WebApp.Pages.Bookings
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Neon.CRM.WebApp.Data.ApplicationDbContext _context;
+
+        public CreateModel(Neon.CRM.WebApp.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id");
+        ViewData["VacationPackageId"] = new SelectList(_context.VacationPackages, "Id", "Id");
+            return Page();
+        }
+
+        [BindProperty]
+        public Booking Booking { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Bookings.Add(Booking);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
