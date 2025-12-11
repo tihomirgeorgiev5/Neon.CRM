7b5f0ba24940391f0e5b3283222b88171d74c9f4
Tihomir Georgiev <georgiev.tm@abv.bg>
2025-12-11 18:00:41 +0100

    added new class in Services - NeonService

---
+++ Neon.CRM.WebApp/Services/NeonService.cs
@@ -0,0 +1,18 @@
+﻿namespace Neon.CRM.WebApp.Services;
+
+public class NeonService
+{
+    private readonly HttpClient _httpClient;
+    private readonly IConfiguration _config;
+
+    public NeonService(HttpClient httpClient, IConfiguration config)
+    {
+        this._httpClient = httpClient;
+        this._config = config;
+
+        _httpClient.BaseAddress = new Uri(_config["NeonApi:BaseUrl"]);
+        _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _config["NeonApi:ApiKey"]);
+    }
+}
+
+
