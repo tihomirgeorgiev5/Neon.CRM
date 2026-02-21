a94e5e97e41bcee5692fba8d51d3baf4521ab423
Tihomir Georgiev <georgiev.tm@abv.bg>
2026-02-21 08:01:40 +0100

    added folder Helpers with EncryptionHelper class

---
+++ Neon.CRM.WebApp/Helpers/EncryptionHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace Neon.CRM.WebApp.Helpers;
+
+public static class EncryptionHelper
+{
+    public static string Encrypt(string plainText, string key)
+    {
+        return plainText;
+    }
+
+    public static string Decrypt(string cipherText, string key)
+    {
+        return cipherText;
+    }
+}
+
+
+
