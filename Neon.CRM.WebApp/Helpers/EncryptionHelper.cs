265ad82c67e51381a6c3612c35618af663a151f7
Tihomir Georgiev <georgiev.tm@abv.bg>
2026-02-21 08:06:45 +0100

    added Encrypt static string for plainText

--- Neon.CRM.WebApp/Helpers/EncryptionHelper.cs
+++ Neon.CRM.WebApp/Helpers/EncryptionHelper.cs
@@ -1,10 +1,25 @@
-﻿namespace Neon.CRM.WebApp.Helpers;
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Neon.CRM.WebApp.Helpers;
 
 public static class EncryptionHelper
 {
     public static string Encrypt(string plainText, string key)
     {
-        return plainText;
+        using var aes = Aes.Create();
+        aes.Key = Encoding.UTF8.GetBytes(key);
+        aes.GenerateIV();
+
+        var encryptor = aes.CreateEncryptor();
+        var plainBytes = Encoding.UTF8.GetBytes(plainText);
+        var encrypted = encryptor.TransformFinalBlock(plainBytes, 0, plainBytes.Length);
+
+        var result = new byte[aes.IV.Length + encrypted.Length];
+        Buffer.BlockCopy(aes.IV, 0, result, 0, aes.IV.Length);
+        Buffer.BlockCopy(encrypted, 0, result, aes.IV.Length, encrypted.Length);
+
+        return Convert.ToBase64String(result);
     }
 
     public static string Decrypt(string cipherText, string key)
