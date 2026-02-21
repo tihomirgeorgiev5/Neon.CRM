19838a32ab7833faaab5ad8ccee74f788f8153ba
Tihomir Georgiev <georgiev.tm@abv.bg>
2026-02-21 08:10:46 +0100

    added Decrypt static string

--- Neon.CRM.WebApp/Helpers/EncryptionHelper.cs
+++ Neon.CRM.WebApp/Helpers/EncryptionHelper.cs
@@ -24,7 +24,21 @@ public static class EncryptionHelper
 
     public static string Decrypt(string cipherText, string key)
     {
-        return cipherText;
+        var fullCipher = Convert.FromBase64String(cipherText);
+        using var aes = Aes.Create();
+        aes.Key = Encoding.UTF8.GetBytes(key);
+
+        var iv = new byte[16];
+        var cipher = new byte[fullCipher.Length - iv.Length];
+
+        Buffer.BlockCopy(fullCipher, 0, iv, 0, iv.Length);
+        Buffer.BlockCopy(fullCipher, iv.Length, cipher, 0, cipher.Length);
+
+        aes.IV = iv;
+        var decryptor = aes.CreateDecryptor();
+
+        var decryptedBytes = decryptor.TransformFinalBlock(cipher, 0, cipher.Length);
+        return Encoding.UTF8.GetString(decryptedBytes);
     }
 }
 
